f81f48750bda0e40f8fe4a5068bac7ac975d376e
Peskinator <78457941+Peskinator@users.noreply.github.com>
2025-10-05 12:53:42 -0400

    Update run.sh

--- maintainerr/run.sh
+++ maintainerr/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/with-contenv bashio
+set -e
 
-bashio::log.info "Starting Maintainerr..."
+bashio::log.info "Starting Maintainerr (Home Assistant Add-on Wrapper)..."
 
 # Read user-configured options
 TZ=$(bashio::config 'TZ')
@@ -9,9 +10,19 @@ API_PORT=$(bashio::config 'API_PORT')
 export TZ
 export API_PORT
 
-# Create symlink from /opt/data to persistent /data
+# Prepare persistent directory
+bashio::log.info "Preparing persistent data directory..."
 mkdir -p /data/maintainerr
-ln -sf /data/maintainerr /opt/data
-bashio::log.info "Linked /opt/data -> /data/maintainerr"
+chmod -R 777 /data/maintainerr || true
 
-exec npm start
+# Remove the ephemeral /opt/data (Docker volume) and replace it with a symlink
+if [ -L /opt/data ] || [ -d /opt/data ]; then
+    rm -rf /opt/data || true
+fi
+
+ln -s /data/maintainerr /opt/data
+bashio::log.info "Linked /opt/data -> /data/maintainerr (persistent)"
+
+# Finally, start the original supervisord process that runs Maintainerr
+bashio::log.info "Launching Maintainerr via supervisord..."
+exec /usr/bin/supervisord -c /etc/supervisord.conf
